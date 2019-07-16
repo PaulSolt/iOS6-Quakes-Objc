f951a927dfebc49754e1560ceaa38d7b12ae638a
Paul Solt <PaulSolt@gmail.com>
2019-07-16 12:11:42 -0400

    Initial project

---
+++ iOS6-Quakes-Objc/iOS6-Quakes-Objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS6-Quakes-Objc
+//
+//  Created by Paul Solt on 7/16/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
